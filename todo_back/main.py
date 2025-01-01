4732a830a9f9b7a4c85aae8d3362d01ecb85e72d
Ana Geguchadze <anageguchadze22@gmail.com>
2025-01-01 17:20:52 +0100

    code for Back End is done

--- todo_back/main.py
+++ todo_back/main.py
@@ -1,6 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-
 
 app = FastAPI()
 
@@ -9,3 +8,37 @@ class Task(BaseModel):
     title: str
     completed: bool
     
+tasks = []
+task_id_counter = 0
+
+@app.get('/tasks')
+def get_tasks():
+    return tasks
+
+
+@app.post('/tasks')
+def create_task(task:Task):
+    global task_id_counter
+    task_id_counter += 1
+    task.id = task_id_counter
+    tasks.append(task.model_dump())
+    return task
+
+
+@app.put('/tasks/{task_id}')
+def update_task(task_id:int, updated_task:Task):
+    for task in tasks:
+        if task['id'] == task_id:
+            if updated_task.title is not None:
+                task['title'] = updated_task.title
+            if updated_task.completed is not None:
+                task['completed'] = updated_task.completed
+            return task
+    raise HTTPException(status_code=404, detail='Task Not Found')
+
+
+@app.delete('/tasks/{task_id}')
+def delete_task(task_id:int):
+    global tasks
+    tasks = [task for task in tasks if task['id'] != task_id]
+    return {'Message': f'Task ID {task_id} Deleted!'}
